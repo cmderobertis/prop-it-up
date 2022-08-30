def503f00d93b16f0488ad13956c9f7d44f04167
cmderobertis <cmderobertis@gmail.com>
2022-08-30 11:37:25 -0700

    refactored for programmatic component rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,25 @@
 import "../App.css"
+import PersonCard from "./PersonCard"
 
 function App() {
-  return <div className="App"></div>
+  const people = [
+    { firstName: "John", lastName: "Smith", age: 88, hairColor: "Green" },
+    { firstName: "Jane", lastName: "Doe", age: 45, hairColor: "Blonde" },
+    { firstName: "Millard", lastName: "Fillmore", age: 50, hairColor: "Blue" },
+    { firstName: "Maria", lastName: "Smith", age: 62, hairColor: "Black" },
+  ]
+  return (
+    <div className="App">
+      {people.map((person) => (
+        <PersonCard
+          firstName={person.firstName}
+          lastName={person.lastName}
+          age={person.age}
+          hairColor={person.hairColor}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,22 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./components/App"
-import PersonCard from "./components/PersonCard"
 import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
     <App />
-    <PersonCard firstName="Jane" lastName="Doe" age="45" hairColor="Black" />
-    <PersonCard firstName="John" lastName="Smith" age="88" hairColor="Black" />
-    <PersonCard
-      firstName="Millard"
-      lastName="Fillmore"
-      age="50"
-      hairColor="Black"
-    />
-    <PersonCard firstName="Maria" lastName="Smith" age="62" hairColor="Black" />
   </React.StrictMode>
 )
 
